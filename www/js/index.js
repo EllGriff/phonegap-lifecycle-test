80e63ce695f9c246b3563a4eb5d36fa7b6159cbc
EllGriff <grie1_17@uni.worc.ac.uk>
2018-10-09 18:04:36 +0100

    Wk 3, Comments added
    comments added, week 3 worksheet

--- www/js/index.js
+++ www/js/index.js
@@ -3,8 +3,8 @@ var resumed_count = 0;
 var launched_count = 0;
 
 document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
+
+
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
@@ -18,27 +18,29 @@ function updateDisplay() {
 	
 	document.addEventListener("resume", onResume, false);
 	document.addEventListener("pause", onPause, false);
-	
+// This is where the evenet listeners are. Defined by the .addEventListener command
+// The lifecycle events are onResume and onPause
+// The event handlers check for different states the phone is in and executes the commands based on the state of the phone
 	launched_count++;
 	updateDisplay();
-	    
+
 	alert("device ready");
     }
 
 
     function onPause() {
-	
+
 	paused_count++;
 	updateDisplay();
-	    
+
 	alert("pause");
     }
-	
+
 
     function onResume() {
-		
+
 	resumed_count++;
 	updateDisplay();
-	    
+
 	alert("resume");
     }
